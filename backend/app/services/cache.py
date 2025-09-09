da9b328dccb288b59fcdc92411a7af0646dc5ef3
Borisov-Evgeniy <evgeniy.borisov@yahoo.com>
2025-09-09 14:31:49 +0300

    add utils

--- backend/app/services/cache.py
+++ backend/app/services/cache.py
@@ -2,7 +2,7 @@ from backend.app.core.redis_client import redis_client
 import json
 
 def cache_set(key: str,value: dict, expire_seconds: int=30):
-    redis_client.setex(key,expire_seconds,json.dump(value))
+    redis_client.setex(key,expire_seconds,json.dumps(value))
 
 def cache_get(key: str):
     res = redis_client.get(key)
